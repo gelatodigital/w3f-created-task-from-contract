e900d0a7b219e745c238051cd6db43bcab4e20f5
Ben Price <ben.jules.price@gmail.com>
2023-06-23 15:19:50 +0100

    Added Comments

--- contracts/Oracle1Balance.sol
+++ contracts/Oracle1Balance.sol
@@ -27,27 +27,41 @@ contract Oracle1Balance is AutomateTaskCreator {
             args: new bytes[](2)
         });
 
+        // Proxy module creates a dedicated proxy for this contract
+        // ensures that only contract created tasks can call certain fuctions
+        // restrict functions by using the onlyDedicatedMsgSender modifier
         moduleData.modules[0] = Module.PROXY;
         moduleData.modules[1] = Module.WEB3_FUNCTION;
 
         moduleData.args[0] = _proxyModuleArg();
         moduleData.args[1] = _web3FunctionModuleArg(
+            // the CID is the hash of the W3f deployed on IPFS
             cid,
+            // the arguments to the W3f are this contracts address
+            // currently W3fs accept string, number, bool as arguments
+            // thus we must convert the address to a string
             abi.encode(Strings.toHexString(address(this)))
         );
 
+        // execData passed to the proxy by the Automate contract
+        // "batchExecuteCall" forwards calls from the proxy to this contract
         bytes memory execData = abi.encodeWithSelector(
             IOpsProxy.batchExecuteCall.selector
         );
 
+        // target address is this contracts dedicatedMsgSender proxy
         taskId = _createTask(
             dedicatedMsgSender,
             execData,
             moduleData,
+            // zero address as fee token indicates
+            // that the contract will use 1Balance for fee payment
             address(0)
         );
     }
 
+    // can only be called by contract created tasks
+    // fee is deducted from 1Balance after execution
     function setNumber(uint256 _number) external onlyDedicatedMsgSender {
         number = _number;
     }
@@ -57,6 +71,7 @@ contract Oracle1Balance is AutomateTaskCreator {
         _cancelTask(taskId);
     }
 
+    // fund executions by depositing to 1Balance
     function depositFunds1Balance(address token, uint256 amount) external payable {
         _depositFunds1Balance(amount, token, address(this));
     }
--- contracts/OracleSyncFee.sol
+++ contracts/OracleSyncFee.sol
@@ -27,33 +27,51 @@ contract OracleSyncFee is AutomateTaskCreator {
             args: new bytes[](2)
         });
 
+        // Proxy module creates a dedicated proxy for this contract
+        // ensures that only contract created tasks can call certain fuctions
+        // restrict functions by using the onlyDedicatedMsgSender modifier
         moduleData.modules[0] = Module.PROXY;
         moduleData.modules[1] = Module.WEB3_FUNCTION;
 
         moduleData.args[0] = _proxyModuleArg();
         moduleData.args[1] = _web3FunctionModuleArg(
+            // the CID is the hash of the W3f deployed on IPFS
             cid,
+            // the arguments to the W3f are this contracts address
+            // currently W3fs accept string, number, bool as arguments
+            // thus we must convert the address to a string
             abi.encode(Strings.toHexString(address(this)))
         );
 
+        // execData passed to the proxy by the Automate contract
+        // "batchExecuteCall" forwards calls from the proxy to this contract
         bytes memory execData = abi.encodeWithSelector(
             IOpsProxy.batchExecuteCall.selector
         );
 
+        // target address is this contracts dedicatedMsgSender proxy
         taskId = _createTask(
             dedicatedMsgSender,
             execData,
             moduleData,
+            // native token as fee token indicates
+            // that the contract will pay the fee synchronously
             NATIVE_TOKEN
         );
     }
 
+    // since we pay synchronously in native tokens
+    // we must be able to deposit to the contract
     // solhint-disable-next-line
     receive() external payable { }
 
+    // can only be called by contract created tasks
     function setNumber(uint256 _number) external onlyDedicatedMsgSender {
         number = _number;
 
+        // since the contract uses sync fee payment it must:
+        // fetch fee, fee token, fee collector from the end of calldata
+        // transfer the appropriate fee to the fee collector
         (uint256 fee, address feeToken) = _getFeeDetails();
         _transfer(fee, feeToken);
     }
