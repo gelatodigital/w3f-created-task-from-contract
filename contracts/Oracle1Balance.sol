0ce861ed86b6c619fab5c65d652a6468cf4c9542
bennoprice <ben.jules.price@gmail.com>
2023-07-20 15:13:02 +0200

    Refactored

--- contracts/Oracle1Balance.sol
+++ contracts/Oracle1Balance.sol
@@ -1,27 +1,55 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-import { Module, ModuleData } from "./vendor/Types.sol";
-import { AutomateTaskCreator } from "./vendor/AutomateTaskCreator.sol";
-import { IOpsProxy } from "./interfaces/IOpsProxy.sol";
-import { NATIVE_TOKEN } from "./constants/Tokens.sol";
-import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+import {Module, ModuleData} from "./vendor/Types.sol";
+import {AutomateTaskCreator} from "./vendor/AutomateTaskCreator.sol";
+import {IOpsProxy} from "./interfaces/IOpsProxy.sol";
+import {NATIVE_TOKEN} from "./constants/Tokens.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Oracle1Balance is AutomateTaskCreator {
     address public owner;
     uint256 public number;
     bytes32 public taskId;
 
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(msg.sender == owner, "Oracle1Balance.onlyOwner");
         _;
     }
 
-    constructor(address automate, string memory cid)
-        AutomateTaskCreator(automate, address(0))
-    {
+    constructor(
+        address automate,
+        string memory cid
+    ) AutomateTaskCreator(automate, address(0)) {
         owner = msg.sender;
+        _createOracleTask(cid);
+    }
 
+    // can only be called by contract created tasks
+    // fee is deducted from 1Balance after execution
+    function setNumber(uint256 _number) external onlyDedicatedMsgSender {
+        number = _number;
+    }
+
+    function cancelTask() external onlyOwner {
+        require(taskId != 0, "Oracle1Balance.cancelTask: task not running");
+        _cancelTask(taskId);
+    }
+
+    function createTask(string memory cid) external onlyOwner {
+        require(taskId == 0, "Oracle1Balance.cancelTask: task already running");
+        _createOracleTask(cid);
+    }
+
+    // fund executions by depositing to 1Balance
+    function depositFunds1Balance(
+        address token,
+        uint256 amount
+    ) external payable {
+        _depositFunds1Balance(amount, token, address(this));
+    }
+
+    function _createOracleTask(string memory cid) internal {
         ModuleData memory moduleData = ModuleData({
             modules: new Module[](2),
             args: new bytes[](2)
@@ -58,21 +86,5 @@ contract Oracle1Balance is AutomateTaskCreator {
             // that the contract will use 1Balance for fee payment
             address(0)
         );
-    }
-
-    // can only be called by contract created tasks
-    // fee is deducted from 1Balance after execution
-    function setNumber(uint256 _number) external onlyDedicatedMsgSender {
-        number = _number;
-    }
-
-    function cancelTask() external onlyOwner {
-        require(taskId != 0, "Oracle1Balance.cancelTask: task not running");
-        _cancelTask(taskId);
-    }
-
-    // fund executions by depositing to 1Balance
-    function depositFunds1Balance(address token, uint256 amount) external payable {
-        _depositFunds1Balance(amount, token, address(this));
     }
 }
--- contracts/OracleSyncFee.sol
+++ contracts/OracleSyncFee.sol
@@ -1,27 +1,62 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-import { Module, ModuleData } from "./vendor/Types.sol";
-import { AutomateTaskCreator } from "./vendor/AutomateTaskCreator.sol";
-import { IOpsProxy } from "./interfaces/IOpsProxy.sol";
-import { NATIVE_TOKEN } from "./constants/Tokens.sol";
-import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+import {Module, ModuleData} from "./vendor/Types.sol";
+import {AutomateTaskCreator} from "./vendor/AutomateTaskCreator.sol";
+import {IOpsProxy} from "./interfaces/IOpsProxy.sol";
+import {NATIVE_TOKEN} from "./constants/Tokens.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 contract OracleSyncFee is AutomateTaskCreator {
     address public owner;
     uint256 public number;
     bytes32 public taskId;
 
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(msg.sender == owner, "OracleSyncFee.onlyOwner");
         _;
     }
 
-    constructor(address automate, string memory cid)
-        AutomateTaskCreator(automate, address(0))
-    {
+    constructor(
+        address automate,
+        string memory cid
+    ) AutomateTaskCreator(automate, address(0)) {
         owner = msg.sender;
+        taskId = _createOracleTask(cid);
+    }
 
+    // since we pay synchronously in native tokens
+    // we must be able to deposit to the contract
+    // solhint-disable-next-line
+    receive() external payable {}
+
+    // can only be called by contract created tasks
+    function setNumber(uint256 _number) external onlyDedicatedMsgSender {
+        number = _number;
+
+        // since the contract uses sync fee payment it must:
+        // fetch fee, fee token, fee collector from the end of calldata
+        // transfer the appropriate fee to the fee collector
+        (uint256 fee, address feeToken) = _getFeeDetails();
+        _transfer(fee, feeToken);
+    }
+
+    function cancelTask() external onlyOwner {
+        require(taskId != 0, "OracleSyncFee.cancelTask: task not running");
+        _cancelTask(taskId);
+    }
+
+    function createTask(string memory cid) external onlyOwner {
+        require(taskId == 0, "OracleSyncFee.cancelTask: task already running");
+        taskId = _createOracleTask(cid);
+    }
+
+    function withdraw(address payable to, uint256 amount) external onlyOwner {
+        (bool sent, ) = to.call{value: amount}("");
+        require(sent, "OracleSyncFee.withdraw: failed to withdraw");
+    }
+
+    function _createOracleTask(string memory cid) internal returns (bytes32) {
         ModuleData memory moduleData = ModuleData({
             modules: new Module[](2),
             args: new bytes[](2)
@@ -50,39 +85,14 @@ contract OracleSyncFee is AutomateTaskCreator {
         );
 
         // target address is this contracts dedicatedMsgSender proxy
-        taskId = _createTask(
-            dedicatedMsgSender,
-            execData,
-            moduleData,
-            // native token as fee token indicates
-            // that the contract will pay the fee synchronously
-            NATIVE_TOKEN
-        );
-    }
-
-    // since we pay synchronously in native tokens
-    // we must be able to deposit to the contract
-    // solhint-disable-next-line
-    receive() external payable { }
-
-    // can only be called by contract created tasks
-    function setNumber(uint256 _number) external onlyDedicatedMsgSender {
-        number = _number;
-
-        // since the contract uses sync fee payment it must:
-        // fetch fee, fee token, fee collector from the end of calldata
-        // transfer the appropriate fee to the fee collector
-        (uint256 fee, address feeToken) = _getFeeDetails();
-        _transfer(fee, feeToken);
-    }
-
-    function cancelTask() external onlyOwner {
-        require(taskId != 0, "OracleSyncFee.cancelTask: task not running");
-        _cancelTask(taskId);
-    }
-
-    function withdraw(address payable to, uint256 amount) external onlyOwner {
-        (bool sent,) = to.call{value: amount}("");
-        require(sent, "OracleSyncFee.withdraw: failed to withdraw");
+        return
+            _createTask(
+                dedicatedMsgSender,
+                execData,
+                moduleData,
+                // native token as fee token indicates
+                // that the contract will pay the fee synchronously
+                NATIVE_TOKEN
+            );
     }
 }
